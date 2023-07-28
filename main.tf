8f189273a65da9b2526642618017e666439ec51f
leefriend99 <lee.friend@oakland-solutions.com>
2023-07-28 09:20:19 +0100

    logging

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "aws_s3_bucket_logging" "this" {
   count = var.disable_access_logging ? 0 : 1
 
   bucket        = aws_s3_bucket.this.id
-  target_bucket = data.aws_s3_bucket.s3_access_log_bucket
+  target_bucket = data.aws_s3_bucket.s3_access_log_bucket.bucket
   target_prefix = "s3_access_logs/${aws_s3_bucket.this.bucket}/log-"
 }
